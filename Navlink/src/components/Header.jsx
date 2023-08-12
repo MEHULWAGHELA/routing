1d379d5798312d0f65ac459a3f083ee39d3e27ff
MEHULWAGHELA <mehuljob123@gmail.com>
2023-08-12 16:44:02 +0530

    new-commit

--- Navlink/src/components/Header.jsx
+++ Navlink/src/components/Header.jsx
@@ -1,34 +1,57 @@
-import { Link, NavLink } from "react-router-dom"
+import { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom"
 
 const Header = (Component) => {
-    let arr = [
-        {
-            path: '/',
-            text: 'Home'
-        },
-        {
-            path: '/About',
-            text: 'About'
-        },
-        {
-            path: '/Contact',
-            text: 'Contact'
-        },
-        {
-            path: '/Service',
-            text: 'Service'
-        },
-    ]
     {/* navlink is add active class on which path we are currently */ }
     const NewComponent = () => {
+        let current = useLocation();
+        let arr = [
+            {
+                path: '/',
+                text: 'Home'
+            },
+            {
+                path: '/About',
+                text: 'About'
+            },
+            {
+                path: '/Contact',
+                text: 'Contact'
+            },
+            {
+                path: '/Service',
+                text: 'Service'
+            },
+        ]
         return (
             <div>
+                {console.log(current)}
                 <div className="container-fluid bg-info">
                     <div className='d-flex justify-content-around p-2'>
-                        {/* <div className="ms-auto p-2 m-2 bg-white"><Link to={'/'} >Home</Link></div>
-                        <div className="p-2 m-2 bg-white"><Link to={'/About'} >About</Link></div>
-                        <div className="p-2 m-2 bg-white"><Link to={'/Contact'}>Contact</Link></div>
-                        <div className="me-auto m-2 p-2 bg-white"><Link to={'/Service'}>Service</Link></div> */}
+                        {arr.map((x, i) => {
+                            return (
+                                <Link key={i} to={x.path}>
+                                    <div className={`p-2 m-2 bg-white ${x.path == current.pathname ? 'color' : ''}`}>
+                                        {x.text}
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <div className="container bg-secondary vh-100 text-white fs-3">
+                        <Component />
+                    </div>
+                </div>
+
+                {/*
+                it is use when navlink use.. when we use 
+                .active {
+                    color: red;
+                } */}
+                {/* <div className="container-fluid bg-info">
+                    <div className='d-flex justify-content-around p-2'>
                         {arr.map((x, i) => {
                             return (
                                 <NavLink to={x.path} >
@@ -37,12 +60,12 @@ const Header = (Component) => {
                                     </div>
                                 </NavLink>
                             )
-                        })}
+                        })} 
                     </div>
                 </div>
                 <div>
                     <Component />
-                </div>
+                </div> */}
             </div>
         )
     }
